57820b31b6889c08908285798c34aa1f7a626d06
GitHub Action <action@github.com>
2022-05-27 18:16:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ivansky/testwebstormplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ivansky.testwebstormplugin.services
+
+import com.github.ivansky.testwebstormplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
